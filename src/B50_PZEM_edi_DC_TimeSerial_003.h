e788db5994e46acda64da980c79d06df2471a511
hosanglee-kr <50106977+hosanglee-kr@users.noreply.github.com>
2024-06-29 21:51:41 +0900

    Update B50_PZEM_edi_DC_TimeSerial_003.h

--- src/B50_PZEM_edi_DC_TimeSerial_003.h
+++ src/B50_PZEM_edi_DC_TimeSerial_003.h
@@ -46,7 +46,11 @@ void B50_PZEM_get_Metrics_PZ003_single(){
 	// and try to check the voltage value
 
 	auto *m = g_B50_PZ003->getMetricsPZ003();		// obtain a pointer to objects metrics
-	//auto *m = g_B50_PZ004->getMetricsPZ004();		// obtain a pointer to objects metrics
+	auto *m = g_B50_PZ004->getMetricsPZ004();		// obtain a pointer to objects metrics
+
+	auto *m = g_B50_PZ004_Dummy->getMetricsPZ004();
+	auto *m = g_B50_PZ003_Dummy->getMetricsPZ003();
+
 
 	// now we should have some response with updated values, let's check again
 	Serial.printf("PZEM voltage: %d (decivolts)\n", m->voltage);
