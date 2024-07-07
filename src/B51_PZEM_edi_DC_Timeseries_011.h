fb8acfb54f9dda2313ee9362e3bd089acb421b36
hosang lee <2540@paran.com>
2024-07-07 13:35:29 +0900

    ///////

--- src/B51_PZEM_edi_DC_Timeseries_011.h
+++ src/B51_PZEM_edi_DC_Timeseries_011.h
@@ -22,11 +22,12 @@ GitHub: https://github.com/vortigont/pzem-edl
 // Pls, check previous examples for basic operations
 // For this example we need an internet connection to get time from NTP server
 
-const char* g_B51_ssid		 		= "your-ssid";
-const char* g_B51_password	 		= "your-password";
+const char* g_B51_ssid		 		= "2G_1";
+const char* g_B51_password	 		= "65641077";
+
 
 const char* g_B51_NTP_ServerURL 	= "pool.ntp.org";
-const char* g_B51_NTP_TIMEZONE	 	= "MSK-3";
+const char* g_B51_NTP_TIMEZONE	 	= "KST-9"; //"MSK-3";
 
 using namespace pz004;	// we will need this namespace for PZEM004v3.0 device
 
@@ -43,17 +44,18 @@ using namespace pz004;	// we will need this namespace for PZEM004v3.0 device
 UartQ*						g_B51_qport;
 
 // Also we need a placeholder for PZEM004 object
-PZ003*						g_B51_pz003;
-DummyPZ003*					g_B51_pz003_dummy;
 
-PZ004*						g_B51_pz004;
+//PZ003*						g_B51_pz003;
+DummyPZ003*						g_B51_pz003;		//g_B51_pz003_dummy;
+
+//PZ004*						g_B51_pz004;
 
 
 // Container object for TimeSeries data
-TSContainer<pz003::metrics> g_B51_TsC_pz003;
-TSContainer<pz003::metrics> g_B51_TsC_pz003_dummy;
+//TSContainer<pz003::metrics> g_B51_TsC_pz003;
+TSContainer<pz003::metrics> 	g_B51_TsC_pz003;		//g_B51_TsC_pz003_dummy;
 
-TSContainer<pz004::metrics> g_B51_TsC_pz004;
+//TSContainer<pz004::metrics> g_B51_TsC_pz004;
 
 // IDs for out time series buffers
 uint8_t						g_B51_TsC_ID_001sec;
@@ -81,17 +83,18 @@ void B51_PZEM_init(){
 
 
 	// Now let's create a PZEM004 object
-	g_B51_pz003 		= new PZ003(G_B51_PZEM_ID_PZ003);
-	g_B51_pz003_dummy 	= new DummyPZ003(G_B51_PZEM_ID_PZ003_DUMMY);
+	g_B51_pz003 		= new DummyPZ003(G_B51_PZEM_ID_PZ003_DUMMY);
+	//g_B51_pz003 		= new PZ003(G_B51_PZEM_ID_PZ003);
+	//g_B51_pz003_dummy 	= new DummyPZ003(G_B51_PZEM_ID_PZ003_DUMMY);
 
-	g_B51_pz004 		= new PZ004(G_B51_PZEM_ID_PZ004);
+	//g_B51_pz004 		= new PZ004(G_B51_PZEM_ID_PZ004);
 
 
 	// and link our port with PZEM object
 	g_B51_pz003->attachMsgQ(g_B51_qport);
-	g_B51_pz003_dummy->attachMsgQ(g_B51_qport);
+	//g_B51_pz003_dummy->attachMsgQ(g_B51_qport);
 
-	g_B51_pz004->attachMsgQ(g_B51_qport);
+	//g_B51_pz004->attachMsgQ(g_B51_qport);
 
 	// one last step - we must start PortQ tasks to handle messages
 	g_B51_qport->startQueues();
@@ -103,17 +106,17 @@ void B51_PZEM_init(){
 		Serial.println("Sorry, can't autopoll somehow :( - g_B51_pz003");
 	}
 
-	if (g_B51_pz003_dummy->autopoll(true)){
-		Serial.println("Autopolling enabled - g_B51_pz003_dummy");
-	} else {
-		Serial.println("Sorry, can't autopoll somehow :( - g_B51_pz003_dummy");
-	}
+	// if (g_B51_pz003_dummy->autopoll(true)){
+	// 	Serial.println("Autopolling enabled - g_B51_pz003_dummy");
+	// } else {
+	// 	Serial.println("Sorry, can't autopoll somehow :( - g_B51_pz003_dummy");
+	// }
 
-	if (g_B51_pz004->autopoll(true)){
-		Serial.println("Autopolling enabled - g_B51_pz004");
-	} else {
-		Serial.println("Sorry, can't autopoll somehow :( - g_B51_pz004");
-	}
+	// if (g_B51_pz004->autopoll(true)){
+	// 	Serial.println("Autopolling enabled - g_B51_pz004");
+	// } else {
+	// 	Serial.println("Sorry, can't autopoll somehow :( - g_B51_pz004");
+	// }
 
 	Serial.println("===");
 
@@ -151,7 +154,8 @@ void B51_init() {
 
 	sntp_set_time_sync_notification_cb(B51_NTP_sync_cb_timeseries);
 
-	B51_WIFI_Connect();
+
+	//// B51_WIFI_Connect();
 
 
 }
@@ -175,7 +179,8 @@ void B51_NTP_sync_cb_timeseries(struct timeval* t) {
 	// total 300 samples will keep per-second data for the duration of 5 minute, then it will roll-over.
 	// Each sample takes about 28 bytes of (SPI)-RAM, it's not a problem to store thouthands if you have SPI-RAM
 
-	g_B51_TsC_ID_001sec = g_B51_TsC_pz004.addTS(
+	g_B51_TsC_ID_001sec = g_B51_TsC_pz003.addTS(
+	//g_B51_TsC_ID_001sec = g_B51_TsC_pz004.addTS(
 									  300						// s - number of entries to keep
 									, time(nullptr) 			// current timestamp : start_time - timestamp of TS creation
 									, 1 						// second interval period - sampling period, all samples that are pushed to TS with lesser interval will be either dropped or averaged if averaging function is provided
@@ -185,7 +190,8 @@ void B51_NTP_sync_cb_timeseries(struct timeval* t) {
 	Serial.printf("Add per-second TimeSeries, id: %d\n", g_B51_TsC_ID_001sec);
 
 	// the same for 30-seconds interval, 240 samples totals, will keep data for 120 min
-	g_B51_TsC_ID_030sec = g_B51_TsC_pz004.addTS(
+	g_B51_TsC_ID_030sec = g_B51_TsC_pz003.addTS(
+	//g_B51_TsC_ID_030sec = g_B51_TsC_pz004.addTS(
 									  240						// s - number of entries to keep
 									, time(nullptr) 			// current timestamp
 									, 30 						// second interval
@@ -195,7 +201,8 @@ void B51_NTP_sync_cb_timeseries(struct timeval* t) {
 	Serial.printf("Add 30 second TimeSeries, id: %d\n", g_B51_TsC_ID_030sec);
 
 	// the same for 300-seconds interval, 288 samples totals, will keep data for 24 hours
-	g_B51_TsC_ID_300sec = g_B51_TsC_pz004.addTS(
+	g_B51_TsC_ID_300sec = g_B51_TsC_pz003.addTS(
+	//g_B51_TsC_ID_300sec = g_B51_TsC_pz004.addTS(
 									  288
 									, time(nullptr) 			// current timestamp
 									, 300 						// second interval
@@ -210,13 +217,16 @@ void B51_NTP_sync_cb_timeseries(struct timeval* t) {
 	//  Now I need to hookup to the PZEM object, it autopolls every second so I can use it's callback
 	//  to collect metrics data and push it to TSContainer
 	// 이제 PZEM 개체에 연결해야 합니다. 매초 자동 폴링되므로 콜백을 사용하여 메트릭 데이터를 수집하고 TSContainer에 푸시할 수 있습니다.
-	auto v_TsC_pz004_ref = &g_B51_TsC_pz004;
+	auto v_TsC_pz003_ref = &g_B51_TsC_pz003;
+	//auto v_TsC_pz004_ref = &g_B51_TsC_pz004;
 
 	// a ref of our Container to feed it to lambda function
-	g_B51_pz004->attach_rx_callback([v_TsC_pz004_ref](uint8_t pzid, const RX_msg* m) {
+	g_B51_pz003->attach_rx_callback([v_TsC_pz003_ref](uint8_t pzid, const RX_msg* m) {
+	//g_B51_pz004->attach_rx_callback([v_TsC_pz004_ref](uint8_t pzid, const RX_msg* m) {
 		// obtain a pointer to objects metrics and push data to TS container marking it with current timestamp
 		// objects 메트릭에 대한 포인터를 얻고 현재 타임스탬프로 표시하는 TS 컨테이너에 데이터를 푸시합니다.
-		v_TsC_pz004_ref->push(*(g_B51_pz004->getMetricsPZ004()), time(nullptr));
+		v_TsC_pz003_ref->push(*(g_B51_pz003->getMetricsPZ003()), time(nullptr));
+		//v_TsC_pz004_ref->push(*(g_B51_pz004->getMetricsPZ004()), time(nullptr));
 	});
 
 	// here I will set a timer to do printing task to serial
@@ -243,7 +253,8 @@ void B51_print_timeseries() {
 	// Process 1 sec data
 
 	// get a ptr to TimeSeries buffer
-	auto   v_ts	= g_B51_TsC_pz004.getTS(g_B51_TsC_ID_001sec);
+	auto   v_ts	= g_B51_TsC_pz003.getTS(g_B51_TsC_ID_001sec);
+	//auto   v_ts	= g_B51_TsC_pz004.getTS(g_B51_TsC_ID_001sec);
 
 	// get const iterator pointing to the begining of the buffer, i.e. the oldest data sample
 	auto   v_iter = v_ts->cbegin();
@@ -259,6 +270,8 @@ void B51_print_timeseries() {
 	Serial.printf("TimeSeries buffer %s has %d items, will only get last %d\n", v_ts->getDescr(), v_ts->getSize(), v_cnt);
 
 	// let's run the iterator and print sampled data
+
+	Serial.println("TimeStamp\tdV\tmA\tW\tWh\tdHz\tpf");
 	for (v_iter; v_iter != v_ts->cend(); ++v_iter) {
 
 		// now I need to get the timestamp for each sample
@@ -267,31 +280,48 @@ void B51_print_timeseries() {
 
 		auto v_timestamp_lastupdate = v_ts->getTstamp() - (v_ts->cend() - v_iter) * v_ts->getInterval();
 
-		Serial.println("TimeStamp\tdV\tmA\tW\tWh\tdHz\tpf");
 		Serial.print(v_timestamp_lastupdate);	Serial.print("\t");
 		Serial.print(v_iter->voltage);			Serial.print("\t");
 		Serial.print(v_iter->current);			Serial.print("\t");
 		Serial.print(v_iter->power);			Serial.print("\t");
 		Serial.print(v_iter->energy);			Serial.print("\t");
-		Serial.print(v_iter->freq);				Serial.print("\t");
-		Serial.print(v_iter->pf);				Serial.println();
+		//// Serial.print(v_iter->freq);				Serial.print("\t");
+		//// Serial.print(v_iter->pf);
+		Serial.println();
 	}
 
 	// Let's do same for 30 sec TimeSeries
 	// just to make it a bit differetnt let't print human-readable date/time instead of time-stamp
 
-	v_ts	 = g_B51_TsC_pz004.getTS(g_B51_TsC_ID_030sec);
+	v_ts	 = g_B51_TsC_pz003.getTS(g_B51_TsC_ID_030sec);
+	//v_ts	 = g_B51_TsC_pz004.getTS(g_B51_TsC_ID_030sec);
 	v_iter 	 = v_ts->cbegin();
 	v_cnt	 = 10;	// we need only 10 samples
 	v_iter	+= v_ts->getSize() - v_cnt;
 
 	Serial.println();
 	Serial.printf("TimeSeries buffer %s has %d items, will only get last %d\n", v_ts->getDescr(), v_ts->getSize(), v_cnt);
+
+	Serial.println("Date/time\t\tdV\tmA\tW\tWh\tdHz\tpf");
 	for (v_iter; v_iter != v_ts->cend(); ++v_iter) {
 		std::time_t v_timestamp_lastupdate	= v_ts->getTstamp() - (v_ts->cend() - v_iter) * v_ts->getInterval();
-		char*		v_dtime_char 			= std::ctime(&v_timestamp_lastupdate);
+		//char*		v_dtime_char 			= std::ctime(&v_timestamp_lastupdate);
 
-		Serial.println("Date/time\tdV\tmA\tW\tWh\tdHz\tpf");
+		struct tm v_tmstruct;
+		char v_dtime_char[30];
+
+		localtime_r(&v_timestamp_lastupdate, &v_tmstruct);
+
+		//getLocalTime(&tmstruct, 5000);
+		sprintf(v_dtime_char,"%d-%02d-%02d %02d:%02d:%02d",
+						(v_tmstruct.tm_year)+1900,
+						(v_tmstruct.tm_mon)+1,
+						v_tmstruct.tm_mday,
+						v_tmstruct.tm_hour,
+						v_tmstruct.tm_min,
+						v_tmstruct.tm_sec
+					);
+
 		Serial.print(v_dtime_char);
 		Serial.print("\t");
 		Serial.print(v_iter->voltage);
@@ -301,15 +331,16 @@ void B51_print_timeseries() {
 		Serial.print(v_iter->power);
 		Serial.print("\t");
 		Serial.print(v_iter->energy);
-		Serial.print("\t");
-		Serial.print(v_iter->freq);
-		Serial.print("\t");
-		Serial.print(v_iter->pf);
+		//// Serial.print("\t");
+		//// Serial.print(v_iter->freq);
+		//// Serial.print("\t");
+		//// Serial.print(v_iter->pf);
 		Serial.println();
 	}
 
 	// Same for 5 min TimeSeries
-	v_ts	 = g_B51_TsC_pz004.getTS(g_B51_TsC_ID_030sec);
+	v_ts	 = g_B51_TsC_pz003.getTS(g_B51_TsC_ID_030sec);
+	//v_ts	 = g_B51_TsC_pz004.getTS(g_B51_TsC_ID_030sec);
 	v_iter	 = v_ts->cbegin();
 	v_cnt	 = 10;											// we need only 10 samples
 	v_iter	+= v_ts->getSize() - v_cnt;
@@ -317,18 +348,39 @@ void B51_print_timeseries() {
 	Serial.println();
 	Serial.printf("TimeSeries buffer %s has %d items, will only get last %d\n", v_ts->getDescr(), v_ts->getSize(), v_cnt);
 
+	Serial.println("Date/time\t\tdV\tmA\tW\tWh\tdHz\tpf");
 	for (v_iter; v_iter != v_ts->cend(); ++v_iter) {
 		std::time_t v_timestamp_lastupdate	= v_ts->getTstamp() - (v_ts->cend() - v_iter) * v_ts->getInterval();
-		char*		v_dtime_char 				= std::ctime(&v_timestamp_lastupdate);
 
-		Serial.println("Date/time\tdV\tmA\tW\tWh\tdHz\tpf");
+		//char*		v_dtime_char 				= std::ctime(&v_timestamp_lastupdate);
+
+
+		struct tm v_tmstruct;
+		char v_dtime_char[30];
+
+		localtime_r(&v_timestamp_lastupdate, &v_tmstruct);
+
+		//getLocalTime(&tmstruct, 5000);
+		sprintf(v_dtime_char,"%d-%02d-%02d %02d:%02d:%02d",
+						(v_tmstruct.tm_year)+1900,
+						(v_tmstruct.tm_mon)+1,
+						v_tmstruct.tm_mday,
+						v_tmstruct.tm_hour,
+						v_tmstruct.tm_min,
+						v_tmstruct.tm_sec
+					);
+		//Serial.println(String(v_dtime_char));
+
+
 		Serial.print(v_dtime_char); 		Serial.print("\t");
+		//Serial.print(v_dtime_char); 		Serial.print("\t");
 		Serial.print(v_iter->voltage);		Serial.print("\t");
 		Serial.print(v_iter->current);		Serial.print("\t");
 		Serial.print(v_iter->power);		Serial.print("\t");
 		Serial.print(v_iter->energy);		Serial.print("\t");
-		Serial.print(v_iter->freq);			Serial.print("\t");
-		Serial.print(v_iter->pf);			Serial.println();
+		//// Serial.print(v_iter->freq);			Serial.print("\t");
+		//// Serial.print(v_iter->pf);			Serial.println();
+		Serial.println();
 	}
 
 	// An example on how to export TS data in json format via WebServer pls see ESPEM Project https://github.com/vortigont/espem
@@ -404,3 +456,29 @@ void B51_ESP32_MEMORY_info_print(){
 	Serial.printf("Flash Size %d, Flash Speed %d\n"					, ESP.getFlashChipSize(), ESP.getFlashChipSpeed());
 }
 
+
+
+
+// #include <WiFi.h>
+// #include <WiFiMulti.h>
+
+// typedef struct {
+// 		String		SSID;
+// 		String		PWD;
+// } B10_T2_WIFI_LOGIN_t;
+
+
+// const uint8_t			g_W10_T2_Wifi_Login_Arr_Size 	= 5;
+
+// B10_T2_WIFI_LOGIN_t		g_W10_T2_Wifi_Login_Arr[g_W10_T2_Wifi_Login_Arr_Size] = {
+// 													  { .SSID = "GalNote10-2540"	, .PWD = "91932542"		}
+// 													, { .SSID = "2G_1"				, .PWD = "65641077"		}
+// 													, { .SSID = "LSITC_WIFI_AP"		, .PWD = "1234554321"	}
+// 													, { .SSID = "U+Net83BF"			, .PWD = "010C020032"	}
+// 													, { .SSID = "LGU+_M200_732675"	, .PWD = "55070016"		}
+// 												};
+
+
+
+
+// WiFiMulti   			g_W10_T2_WIfiMulti;
